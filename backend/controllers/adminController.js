29ab48517650199ef47e9e1d2f731426e891aa49
YOUNESS AKARID <149713595+younessakarid@users.noreply.github.com>
2025-05-13 14:14:16 +0100

    commit

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,13 @@
+
+
+// API for adding doctor
+const addDoctor = async (req,res) => {
+    try {
+        const { name, email, password, speciality, degree, experience, about, fees, address } = req.body
+       
+    } catch (error) {
+        
+    }
+}
+
+export {addDoctor}
