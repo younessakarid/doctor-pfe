ea5dd92783af898d13156319c294bbd49fee65d3
YOUNESS AKARID <149713595+younessakarid@users.noreply.github.com>
2025-05-13 15:33:30 +0100

    commit

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -4,7 +4,9 @@
 const addDoctor = async (req,res) => {
     try {
         const { name, email, password, speciality, degree, experience, about, fees, address } = req.body
-       
+        const imageFile = req.imageFile
+        
+        console.log({ name, email, password, speciality, degree, experience, about, fees, address },imageFile);
     } catch (error) {
         
     }
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
 
 
 // app config
@@ -16,7 +17,11 @@ connectCloudinary()
 app.use(express.json())
 app.use(cors())
 
+
 // api endpoints
+app.use('/api/admin',adminRouter)
+
+
 
 app.get('/',(req,res)=> {
     res.send('api working')
