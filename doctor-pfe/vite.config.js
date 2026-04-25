df2a65f60451dac5c5726f4cd66047645dbad3c2
younesakarid <149713595+younessakarid@users.noreply.github.com>
2026-04-25 18:32:54 +0200

    fix: add react plugin to vite config

--- doctor-pfe/vite.config.js
+++ doctor-pfe/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [
-    
+    react(),
     tailwindcss(),
   ],
-    server:{port:5173}
-  
+  server: { port: 5173 },
+  optimizeDeps: {
+    include: ['react-toastify']
+  }
 })
-
-
